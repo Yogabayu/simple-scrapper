130dfd802033b8cc82f1d896cfa5b240fe4ae7f7
Yogabayu <yogabayusbi@gmail.com>
2024-07-24 16:44:18 +0700

    get next page data

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
 import puppeteer from "puppeteer";
 
 const getQuotes = async () => {
-  // Start a Puppeteer session with:
-  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
   });
 
-  // Open a new page
   const page = await browser.newPage();
-
-  // On this new page:
-  // - open the "http://quotes.toscrape.com/" website
-  // - wait until the dom content is loaded (HTML is ready)
   await page.goto("http://quotes.toscrape.com/", {
     waitUntil: "domcontentloaded",
   });
 
-  // Get page data
-  const quotes = await page.evaluate(() => {
-    // Fetch all elements with class "quote"
-    const quoteElements = document.querySelectorAll(".quote");
+  const quotesArray = [];
 
-    // Create an array to store the quotes
-    const quotesArray = [];
+  let hasNextPage = true;
+  while (hasNextPage) {
+    // Get page data
+    const quotes = await page.evaluate(() => {
+      const quoteElements = document.querySelectorAll(".quote");
+      const quotesOnPage = [];
 
-    // Iterate over each element and extract the text and author
-    quoteElements.forEach((quote) => {
-      const text = quote.querySelector(".text").innerText;
-      const author = quote.querySelector(".author").innerText;
-      quotesArray.push({ text, author });
+      quoteElements.forEach((quote) => {
+        const text = quote.querySelector(".text").innerText;
+        const author = quote.querySelector(".author").innerText;
+        quotesOnPage.push({ text, author });
+      });
+
+      return quotesOnPage;
     });
 
-    return quotesArray;
-  });
+    quotesArray.push(...quotes);
+
+    // Check if there is a next page
+    hasNextPage = await page.evaluate(() => {
+      const nextButton = document.querySelector(".pager > .next > a");
+      if (nextButton) {
+        nextButton.click();
+        return true;
+      }
+      return false;
+    });
+
+    // Wait for the next page to load
+    if (hasNextPage) {
+      await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+    }
+  }
 
   // Display the quotes
-  console.log(quotes);
+  console.log(quotesArray);
 
-  // Close the browser
   await browser.close();
 };
 
